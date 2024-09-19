c4a5c12149e1f516855038106f1e33f5eec0c88d
AppleBallCat <puskarkc@gmail.com>
2024-09-19 22:08:34 +1000

    Update tofire.js

--- tofire.js
+++ tofire.js
@@ -1,6 +1,6 @@
 // Your web app's Firebase configuration
 
-console.log("JS File Loaded -- tofire.js 9:52");
+console.log("JS File Loaded -- tofire.js 10:9");
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getDatabase, ref, set, get, child, remove, update } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
